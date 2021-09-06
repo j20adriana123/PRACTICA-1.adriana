f967ae94d6d65da439f17f9afabebc8fad7e6a43
ADRIANA <89797804+j20adriana123@users.noreply.github.com>
2021-09-06 12:50:02 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+fuction get imc(){
+  var peso=parce float(document.get Elementby id("peso),value);
+  var altura= parcefloat(document.getElementby id (altura).value);
+  var imc=peso/(altura*altura);
+  documentgetElementby id ("imc").innerhtml=imc;
